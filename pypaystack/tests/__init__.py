cffcd7b859eedd6cae0b1e69ca40adf67f392b5e
Edward Popoola <edward@cowrywise.com>
2020-07-20 02:51:20 -0600

    remove duplicate test folder

--- pypaystack/tests/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-import time
-from unittest import TestCase
-from uuid import uuid4
-
-from pypaystack import Customer, Plan, Transaction
-
-test_auth_key = os.getenv('PAYSTACK_AUTHORIZATION_KEY')
